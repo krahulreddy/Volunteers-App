1e94a2124e085cb59466e2e2c2fef96dd1ac6b56
Neeramitra <neeramitra.reddy@gmail.com>
2019-03-17 14:35:00 +0530

    Integrated maps into the application.

--- Volunteer9&/app/src/main/java/com/selab/volunteer/HostAnEvent.java
+++ Volunteer9&/app/src/main/java/com/selab/volunteer/HostAnEvent.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,17 +18,25 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class HostAnEvent extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     DatabaseReference databaseEvents;
 
 
-    EditText EventName,EventDesc,EventTimeHours,EventTimeMinutes,EventPay,EventLocation;
+    EditText EventName,EventDesc,EventPay;
     TextView EventDate;
+    Date eventdate;
     private Button nextbutton = null;
     private Button GoToCalender=null;
-    private Button cancelhost;
+    private Button cancelhost=null;
+    int EventTimeHours,EventTimeMinutes;
     EventOneSchema eventOneSchema,formax;
 
     long maxid = 2;
@@ -35,24 +44,26 @@ public class HostAnEvent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hostanevent);
         nextbutton = (Button) findViewById(R.id.NextStep);
         EventName=(EditText)findViewById(R.id.HostAnEventName);
-        EventLocation=(EditText)findViewById(R.id.HostAnEventLocation);
         EventDate=(TextView) findViewById(R.id.HostAnEventDate);
         EventDesc=(EditText)findViewById(R.id.HostAnEventDescription);
-        EventTimeHours=(EditText)findViewById(R.id.HostAnEventTimeHours);
-        EventTimeMinutes=(EditText)findViewById(R.id.HostAnEventTimeMinutes);
+        TimePicker HostAnEventTime=(TimePicker)findViewById(R.id.HostAnEventTime) ;
         EventPay=(EditText)findViewById(R.id.HostAnEventMoney);
         GoToCalender=(Button)findViewById(R.id.GoToCalender);
-        cancelhost = findViewById(R.id.CancelHost);
-
-        mAuth = FirebaseAuth.getInstance();
-
-
         Intent incoming = getIntent();
         final String date = incoming.getStringExtra("date");
         EventDate.setText(date);
+        HostAnEventTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(getApplicationContext(), hourOfDay + "  " + minute, Toast.LENGTH_SHORT).show();
+                EventTimeHours=hourOfDay;
+                EventTimeMinutes=minute;
+
+            }
+        });
+        mAuth=FirebaseAuth.getInstance();
 
         GoToCalender.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,23 +83,33 @@ public class HostAnEvent extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 final String eventname=EventName.getText().toString();
-                final String eventdate=EventDate.getText().toString();
                 final String eventdesc=EventDesc.getText().toString();
                 final String eventpay=EventPay.getText().toString();
-                final String eventtimehours=EventTimeHours.getText().toString();
-                final String eventtimeminutes=EventTimeMinutes.getText().toString();
-                final String eventlocation=EventLocation.getText().toString();
+                //Have to remove this hardcoded location and add money to the database
+                final String eventlocation="nitk";
+                Date presdate= Calendar.getInstance().getTime();
+                try {
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                    eventdate= df.parse(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
                 if(eventname.length()==0)
                 {
                     EventName.requestFocus();
                     EventName.setError("NAME CANNOT BE EMPTY!");
+                }
+                else if(presdate.after(eventdate))
+                {
+                    EventDate.requestFocus();
+                    EventDate.setError("EVENT DATE CANNOT BE A PAST DATE!");
                 }
                 else if(!eventname.matches("[a-zA-z ]*+"))
                 {
                     EventName.requestFocus();
                     EventName.setError("ONLY ALPHABETICAL CHARACTERS ALLOWED!");
                 }
-                else if(eventdate.length()==0) {
+                else if(date.length()==0) {
                     EventDate.requestFocus();
                     EventDate.setError("SELECT A DATE!");
                 }
@@ -98,43 +119,17 @@ public class HostAnEvent extends AppCompatActivity {
                     EventPay.setError("PAYMENT CANNOT BE EMPTY!");
 
                 }
-                else if(eventtimeminutes.length()==0)
-                {
-                    EventTimeMinutes.requestFocus();
-                    EventTimeMinutes.setError("MINUTES CANNOT BE EMPTY!");
-
-                }
-                else if(Integer.parseInt(eventtimeminutes)>60 || Integer.parseInt(eventtimeminutes)<0)
-                {
-                    EventTimeMinutes.requestFocus();
-                    EventTimeMinutes.setError("INVALID TIME");
-                }
-                else if(eventtimehours.length()==0)
-                {
-                    EventTimeHours.requestFocus();
-                    EventTimeHours.setError("HOURS CANNOT BE EMPTY!");
-                }
-                else if(Integer.parseInt(eventtimehours)>24 || Integer.parseInt(eventtimehours)<0)
-                {
-                    EventTimeHours.requestFocus();
-                    EventTimeHours.setError("INVALID TIME");
-                }
                 else if(eventdesc.length()==0)
                 {
                     EventDesc.requestFocus();
                     EventDesc.setError("DESCRIPTION CANNOT BE EMPTY!");
 
                 }
-                else if(eventlocation.length()==0)
-                {
-                    EventLocation.requestFocus();
-                    EventLocation.setError("LOCATION CANNOT BE EMPTY");
-                }
                 else {
                     Toast.makeText(getApplicationContext(),"Event information saved!",Toast.LENGTH_LONG).show();
-                    //Database starts herer
+                    //Database starts here
 
-                    final EventOneSchema eventOneSchema = new EventOneSchema(eventname,eventdate,eventlocation,eventdesc, Integer.parseInt(eventpay));
+                    final EventOneSchema eventOneSchema = new EventOneSchema(eventname,date,eventlocation,eventdesc, Integer.parseInt(eventpay));
 
 
 

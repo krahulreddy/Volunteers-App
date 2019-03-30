b25748b13e34c0608ad747d0c30fed71bde646f2
rcs10 <dhanueshrc28@gmail.com>
2019-03-30 14:33:00 +0530

    Internet Resolved and ProgressBar Removed

--- app/src/main/java/com/selab/volunteer/Login.java
+++ app/src/main/java/com/selab/volunteer/Login.java
@@ -1,6 +1,9 @@
 package com.selab.volunteer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -36,8 +40,9 @@ public class Login extends AppCompatActivity {
 
     EditText Username;
     EditText Password;
-    ProgressBar progress;
     LinearLayout layout;
+    boolean connected = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,8 +53,6 @@ public class Login extends AppCompatActivity {
 
 
         layout=(LinearLayout)findViewById(R.id.loginlayout);
-        progress=(ProgressBar) findViewById(R.id.loginprogressbar);
-        progress.setVisibility(View.GONE);
         layout.setVisibility(View.VISIBLE);
         Toolbar toolbar= findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -85,72 +88,45 @@ public class Login extends AppCompatActivity {
                 });
                 //intent.putExtra("CurrentUserId", mAuth.getUid() );
                 startActivity(intent);
-                progress.setVisibility(View.GONE);
-                layout.setVisibility(View.VISIBLE);
                 //Toast.makeText(getApplicationContext(), "Login Successful!!", Toast.LENGTH_LONG).show();
             }
         }
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flag=0;
-                email=Username.getText().toString();
-                password=Password.getText().toString();
-                if (TextUtils.isEmpty(email)) {
-                    Username.setError("Enter Username");
-                    flag=1;
+
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                    //we are connected to a network
+                    connected = true;
                 }
-                if(TextUtils.isEmpty(password)) {
-                    Password.setError("Enter Password");
-                    flag=1;
-                }
+                else
+                    connected = false;
 
-                if(flag==0)
-                {
-                    progress.setVisibility(View.VISIBLE);
-                    layout.setVisibility(View.GONE);
-                    //if(mAuth.getCurrentUser().)
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(com.selab.volunteer.Login.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
+                if (connected == true) {
+                    flag = 0;
+                    email = Username.getText().toString();
+                    password = Password.getText().toString();
+                    if (TextUtils.isEmpty(email)) {
+                        Username.setError("Enter Username");
+                        flag = 1;
+                    }
+                    if (TextUtils.isEmpty(password)) {
+                        Password.setError("Enter Password");
+                        flag = 1;
+                    }
 
-                            if(task.isSuccessful()){
-                                if(!mAuth.getCurrentUser().isEmailVerified())
-                                {
-                                    mAuth.getCurrentUser().sendEmailVerification();
-                                    Toast.makeText(getApplicationContext(), "Verify " + mAuth.getCurrentUser().getEmail() + " and then login", Toast.LENGTH_LONG).show();
-                                }
-                                else {
-                                    Intent intent = new Intent(Login.this, MainActivity.class);
-                                    // Toast.makeText(Login.this, mAuth.getUid(), Toast.LENGTH_SHORT).show();
-                                    DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getUid()).child("name");
-
-                                    reff.addValueEventListener(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                            Toast.makeText(Login.this, ""+ dataSnapshot.getValue(), Toast.LENGTH_SHORT).show();
-                                        }
-
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                        }
-                                    });
-                                    //intent.putExtra("CurrentUserId", mAuth.getUid() );
-                                    startActivity(intent);
-                                    progress.setVisibility(View.GONE);
-                                    layout.setVisibility(View.VISIBLE);
-                                    //Toast.makeText(getApplicationContext(), "Login Successful!!", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                            else
-                            {
-                                layout.setVisibility(View.VISIBLE);
-                                progress.setVisibility(View.GONE);
-                                Toast.makeText(getApplicationContext(), "Error!!\nLogin Unsuccessful", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
+                    if (flag == 0) {
+                        setup();
+                    }
+                } else {
+                    Toast.makeText(Login.this, "Check Internet Connectivity", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -176,5 +152,48 @@ public class Login extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         startActivity(new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    private void setup()
+    {
+        //progress.setVisibility(View.VISIBLE);
+        //layout.setVisibility(View.GONE);
+        //if(mAuth.getCurrentUser().)
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(com.selab.volunteer.Login.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    if (!mAuth.getCurrentUser().isEmailVerified()) {
+                        Toast.makeText(getApplicationContext(), "Verify " + mAuth.getCurrentUser().getEmail() + " and then login", Toast.LENGTH_LONG).show();
+                    } else {
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        // Toast.makeText(Login.this, mAuth.getUid(), Toast.LENGTH_SHORT).show();
+                        DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getUid()).child("name");
+
+                        reff.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                Toast.makeText(Login.this, "" + dataSnapshot.getValue(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                        //intent.putExtra("CurrentUserId", mAuth.getUid() );
+                        startActivity(intent);
+                        //                        progress.setVisibility(View.GONE);
+                        //                          layout.setVisibility(View.VISIBLE);
+                        //Toast.makeText(getApplicationContext(), "Login Successful!!", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    //                        layout.setVisibility(View.VISIBLE);
+                    //                          progress.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), "Error!!\nLogin Unsuccessful", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }

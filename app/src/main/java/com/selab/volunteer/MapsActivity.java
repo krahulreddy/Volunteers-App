57615e35d081c9e1aace096d0173f5e4863c9787
Neeramitra <neeramitra.reddy@gmail.com>
2019-04-02 02:27:27 +0530

    Host can't register his own event

--- app/src/main/java/com/selab/volunteer/MapsActivity.java
+++ app/src/main/java/com/selab/volunteer/MapsActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
